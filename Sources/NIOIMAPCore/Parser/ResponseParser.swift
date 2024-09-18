1016f767dfef456807568fada538605a1f331f89
Jordan Mendelson <jordan@empathy.email>
2024-09-18 16:00:44 -0700

    Update package dependencie and set default ResponseParser bufferLimit to 10 MB

--- Sources/NIOIMAPCore/Parser/ResponseParser.swift
+++ Sources/NIOIMAPCore/Parser/ResponseParser.swift
@@ -62,7 +62,7 @@ public struct ResponseParser: Parser, Sendable {
     /// Creates a new `ResponseParser`.
     /// - parameter bufferLimit: The maximum amount of data that may be buffered by the parser. If this limit is exceeded then an error will be thrown. Defaults to 1000 bytes.
     public init(
-        bufferLimit: Int = 8_192,
+        bufferLimit: Int = 10_485_760,
         messageAttributeLimit: Int = .max,
         bodySizeLimit: UInt64 = .max,
         literalSizeLimit: Int = IMAPDefaults.literalSizeLimit,
